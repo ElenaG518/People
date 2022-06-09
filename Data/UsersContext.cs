a00443a62b570b13dd5b5371823dfc96579c1947
ElenaG518 <patty518@yahoo.com>
2022-06-09 11:54:11 -0700

    Create db context for users

---
+++ Data/UsersContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using People.Models;
+
+namespace People.Data
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext(DbContextOptions<UsersContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
+
